4e5c8cc5256d7cb625e679ea017ebe5a2f6c659f
sibrahim7071 <sibrahim7071@bths.edu>
2021-01-10 17:50:00 -0500

    added expiration date to storage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,13 @@
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
         // Creates main fridge object and runs it
-        Fridge fridge = new Fridge();
-        fridge.runFridge(args);
-
-        // new StorageManager().saveStorage();
+        // Fridge fridge = new Fridge();
+        // fridge.runFridge(args);
 
         // Intake intake = new Intake();
         // intake.doLaunch(args);
--- src/main/java/StorageManager.java
+++ src/main/java/StorageManager.java
@@ -6,45 +6,64 @@ import org.json.simple.parser.ParseException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class StorageManager {
-    private int[] storage;
+    private int[] ids;
+    private LocalDate[] dates;
 
     public StorageManager() throws IOException, ParseException {
-        storage = setStorage();
-    }
+        ArrayList<Integer> idList = new ArrayList<>();
+        ArrayList<LocalDate> dateList = new ArrayList<>();
 
-    public void add(int id) {
-        int[] array = new int[storage.length + 1];
-        System.arraycopy(storage, 0, array, 0, storage.length);
-        array[storage.length] = id;
-        storage = array;
-    }
-
-    public int[] getStorage() {
-        return storage;
-    }
-
-    private int[] setStorage() throws IOException, ParseException {
-        Iterator<Long> iterator = ((JSONArray) ((JSONObject) new JSONParser().parse(new FileReader(Constants.STORAGE_FILE))).get("ids")).iterator();
-        ArrayList<Integer> list = new ArrayList<>();
-        while (iterator.hasNext()) {
-            list.add(Math.toIntExact(iterator.next()));
+        for (JSONObject item : (Iterable<JSONObject>) ((JSONObject) new JSONParser().parse(new FileReader(Constants.STORAGE_FILE))).get("items")) {
+            idList.add(Math.toIntExact((Long) item.get("id")));
+            dateList.add(LocalDate.parse((String) item.get("expire"), DateTimeFormatter.ofPattern("MM-dd-yyyy")));
         }
-        int[] array = new int[list.size()];
-        for (int i = 0; i < array.length; i++)
-            array[i] = list.get(i);
-        return array;
+
+        ids = new int[idList.size()];
+        dates = new LocalDate[dateList.size()];
+
+        for (int i = 0; i < idList.size(); i++) {
+            ids[i] = idList.get(i);
+            dates[i] = dateList.get(i);
+        }
+    }
+
+    public int[] getIds() {
+        return ids;
+    }
+
+    public LocalDate[] getDates() {
+        return dates;
+    }
+
+    public void add(int id, LocalDate date) {
+        int[] idArray = new int[ids.length + 1];
+        LocalDate[] dateArray = new LocalDate[dates.length + 1];
+
+        System.arraycopy(ids, 0, idArray, 0, ids.length);
+        System.arraycopy(dates, 0, dateArray, 0, dates.length);
+
+        idArray[ids.length] = id;
+        dateArray[dates.length] = date;
+
+        ids = idArray;
+        dates = dateArray;
     }
 
     public void saveStorage() {
         JSONObject object = new JSONObject();
         JSONArray array = new JSONArray();
-        for (int id : storage)
-            array.add(id);
-        object.put("ids", array);
+        for (int i = 0; i < ids.length; i++) {
+            JSONObject item = new JSONObject();
+            item.put("id", ids[i]);
+            item.put("expire", dates[i].format(DateTimeFormatter.ofPattern("MM-dd-yyyy")));
+            array.add(item);
+        }
+        object.put("items", array);
         try {
             FileWriter file = new FileWriter(Constants.STORAGE_FILE);
             file.write(object.toJSONString());
